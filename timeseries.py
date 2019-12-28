73c4c369dad6f99926c7e80ba63c517889901380
Will Graham <will.graham0@gmail.com>
2019-12-28 12:30:29 +0000

    feat: create basic timeseries object

---
+++ timeseries.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from typing import NamedTuple
+
+from utils import pairwise
+
+
+class Point(NamedTuple):
+    """A tuple of datetime and value."""
+    time: datetime
+    value: float
+
+
+class TimeSeries:
+    """A chronologically ordered iterable of Points."""
+
+    def __init__(self, points):
+        self.points = tuple(
+            Point(*point)
+            for point in sorted(points, key=lambda x: x.time)
+        )
+
+    @property
+    def area(self):
+        """Return the area under the time series."""
+        return (
+            sum(
+                (point.value + next_point.value)
+                * (next_point.time - point.time).total_seconds()
+                for point, next_point in pairwise(self.points)
+            )
+            / 2.0
+        )
