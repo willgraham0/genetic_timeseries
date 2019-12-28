af5178491866410f9da7730d405bda7c64c5beb0
Will Graham <will.graham0@gmail.com>
2019-12-28 12:30:08 +0000

    feat: create pairwise util

---
+++ utils.py
@@ -0,0 +1,11 @@
+from itertools import tee
+
+
+def pairwise(iterable):
+    """
+    From the itertools cookbook.
+    s -> (s0,s1), (s1,s2), (s2, s3), ...
+    """
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
