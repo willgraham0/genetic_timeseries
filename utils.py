379ae7e84eebfc0d430004b8c8645787428ab41e
Will Graham <will.graham0@gmail.com>
2019-12-30 17:06:00 +0000

    refactor: remove unused method

--- utils.py
+++ utils.py
@@ -1,6 +1,4 @@
-from datetime import datetime, timedelta
 from itertools import tee
-from typing import List
 
 
 def pairwise(iterable):
@@ -11,10 +9,3 @@ def pairwise(iterable):
     a, b = tee(iterable)
     next(b, None)
     return zip(a, b)
-
-
-def generate_hh_range(start: datetime, end: datetime) -> List[datetime]:
-    """Returns a list of half hourly date-times between start and end."""
-    step = timedelta(minutes=30)
-    hh_count = int((end - start).total_seconds() / step.seconds)
-    return [(start + step * hh) for hh in range(0, hh_count)]
