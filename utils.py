60221d98777515389f2a5f7cc13ec10eb6fc6d37
Will Graham <will.graham0@gmail.com>
2019-12-31 10:47:44 +0000

    feat: implement random datetime function

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
+from datetime import datetime, timedelta
 from itertools import tee
+from random import random
 
 
 def pairwise(iterable):
@@ -9,3 +11,10 @@ def pairwise(iterable):
     a, b = tee(iterable)
     next(b, None)
     return zip(a, b)
+
+
+def random_datetime(start: datetime, end: datetime) -> datetime:
+    """Return a random datetime between the specified start and end datetimes."""
+    time_delta = (end - start).total_seconds()
+    random_time_delta = time_delta * random()
+    return start + timedelta(seconds=random_time_delta)
