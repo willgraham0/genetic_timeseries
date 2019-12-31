2e75737011b8a8c1d5d298f55f35857aff1e47e9
Will Graham <will.graham0@gmail.com>
2019-12-31 12:24:52 +0000

    feat: implement str method on Point

--- timeseries.py
+++ timeseries.py
@@ -16,6 +16,9 @@ class Point:
         """Return a value that represents the vector distance between two points."""
         return sqrt(((self.time - other.time).total_seconds())**2 + (self.value - other.value)**2)
 
+    def __str__(self):
+        return f"<Point: time={self.time} value={self.value}>"
+
 
 class TimeSeries:
     """A chronologically ordered iterable of Points."""
