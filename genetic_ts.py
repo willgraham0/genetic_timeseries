25f3e18655f1b45ba9d7122bcdb6ca4bf8920970
Will Graham <will.graham0@gmail.com>
2019-12-31 09:57:00 +0000

    feat: implement properties

--- genetic_ts.py
+++ genetic_ts.py
@@ -1,42 +1,63 @@
 from __future__ import annotations
+from datetime import datetime
 from typing import Tuple, Type
 
 from environment import Environment
-from timeseries import TimeSeries
+from timeseries import Point
 from replicator import Replicator
 
 
 class GeneticTimeSeries(Replicator):
 
-    ideal: TimeSeries = None
+    ideal: GeneticTimeSeries = None
+
+    def __init__(self, points) -> None:
+        self.points = tuple(
+            Point(*point)
+            for point in sorted(points, key=lambda x: x.time)
+        )
+
+    @property
+    def max_value(self) -> float:
+        """Return the largest value in the genetic time series."""
+        return max(point.value for point in self.points)
+
+    @property
+    def min_value(self) -> float:
+        """Return the smallest value in the genetic time series."""
+        return min(point.value for point in self.points)
+
+    @property
+    def latest_time(self) -> datetime:
+        """Return the latest time in the genetic time series."""
+        return max(point.time for point in self.points)
+
+    @property
+    def earliest_time(self) -> datetime:
+        """Return the earliest time in the genetic time series."""
+        return min(point.time for point in self.points)
 
     @classmethod
-    def configure(cls, ideal: TimeSeries) -> None:
+    def configure(cls, ideal: GeneticTimeSeries) -> None:
         cls.ideal = ideal
 
     @classmethod
     def is_configured(cls, func):
         if not cls.ideal:
-            raise ValueError("Class must be configured before this method can be called.")
+            raise ValueError("This class must be configured before this method can be called.")
         return func
 
     @is_configured
     def fitness(self) -> float:
         pass
 
-    @is_configured
     def mutate(self) -> None:
         pass
 
-    @is_configured
-    def crossover(
-        self: GeneticTimeSeries,
-        other: GeneticTimeSeries
-    ) -> Tuple[GeneticTimeSeries, GeneticTimeSeries]:
+    def crossover(self, other: GeneticTimeSeries) -> Tuple[GeneticTimeSeries, GeneticTimeSeries]:
         pass
 
     @classmethod
-    @is_configured
     def random_instance(cls: Type[GeneticTimeSeries]) -> GeneticTimeSeries:
         pass
 
